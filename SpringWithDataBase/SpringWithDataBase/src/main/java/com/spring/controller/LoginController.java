d5564621221cac968df5db3c44fc1e6c99a541e6
NilayRai <nilaykumarrai0@gmail.com>
2018-10-11 16:57:35 +0530

    Code Commited

--- SpringWithDataBase/SpringWithDataBase/src/main/java/com/spring/controller/LoginController.java
+++ SpringWithDataBase/SpringWithDataBase/src/main/java/com/spring/controller/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class LoginControllerdemo {
+public class LoginController{
 	@Autowired
 	PersonService personservice;
 
